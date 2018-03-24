77783ac689bf03fec0a7b21183e7459db171bbba
James Waples <james@cambridge-intelligence.com>
2018-03-24 15:20:49 +0000

    Config files and stuff for Xargo build

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,17 @@
+//! Driver for the SSD1331 colour OLED display
+
+#![no_std]
+#![deny(missing_debug_implementations)]
+#![deny(missing_docs)]
+#![deny(warnings)]
+#![deny(missing_copy_implementations)]
+#![deny(trivial_casts)]
+#![deny(trivial_numeric_casts)]
+#![deny(unsafe_code)]
+#![deny(unstable_features)]
+#![deny(unused_import_braces)]
+#![deny(unused_qualifications)]
+
 #[cfg(test)]
 mod tests {
     #[test]
