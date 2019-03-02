bef331ca581a985585597893b38eda00105681ea
James Waples <james@wapl.es>
2019-03-02 20:40:52 +0000

    Doc nit

--- src/displayrotation.rs
+++ src/displayrotation.rs
@@ -1,18 +1,14 @@
 //! Display rotation
 
-// TODO: Add to prelude
-/// Display rotation.
-///
-/// Note that 90º and 270º rotations are not supported by
-// [`TerminalMode`](../mode/terminal/struct.TerminalMode.html).
+/// Display rotation
 #[derive(Clone, Copy)]
 pub enum DisplayRotation {
     /// No rotation, normal display
     Rotate0,
-    /// Rotate by 90 degress clockwise
+    /// Rotate by 90 degrees clockwise
     Rotate90,
-    /// Rotate by 180 degress clockwise
+    /// Rotate by 180 degrees clockwise
     Rotate180,
-    /// Rotate 270 degress clockwise
+    /// Rotate 270 degrees clockwise
     Rotate270,
 }
